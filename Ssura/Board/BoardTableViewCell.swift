447784fd913bb8ba27494fc7026597a66f0f31fb
huyukey <huyukey@gmail.com>
2018-12-24 01:15:12 +0900

    init

---
+++ Ssura/Board/BoardTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BoardTableViewCell.swift
+//  Ssura
+//
+//  Created by 겨울나무 on 23/12/2018.
+//  Copyright © 2018 겨울나무. All rights reserved.
+//
+
+import UIKit
+
+class BoardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Ssura/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ssura
+//
+//  Created by 겨울나무 on 23/12/2018.
+//  Copyright © 2018 겨울나무. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
